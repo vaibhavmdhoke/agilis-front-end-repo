ca9da5cf2888903c4741a5542b4786f109259ef8
Karthik-V26 <vangetikarthik26@gmail.com>
2021-08-31 18:06:21 +0530

    Added route for Karthik Profile

--- index.js
+++ index.js
@@ -29,16 +29,40 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-
 app.get("/user", (req, res) => {
   res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
 app.get("/vikas", (req, res) => {
-  res.render("userResume", { title: "Vikas", userProfile: { image:"/vikas-photo.jpg" ,name: "Vikas", college: "CVR", residence: "Hyderabad", company:"Agilis AS", role: "Front End Engineer",
-  contactNumber:"6300813872", emailId:"msaivikas98@gmail.com"} });
+  res.render("userResume", {
+    title: "Vikas",
+    userProfile: {
+      image: "/vikas-photo.jpg",
+      name: "Vikas",
+      college: "CVR",
+      residence: "Hyderabad",
+      company: "Agilis AS",
+      role: "Front End Engineer",
+      contactNumber: "6300813872",
+      emailId: "msaivikas98@gmail.com",
+    },
+  });
 });
-
+app.get("/karthik", (req, res) => {
+  res.render("userResume", {
+    title: "Karthik",
+    userProfile: {
+      image: "/karthik-photo.jpeg",
+      name: "Karthik",
+      college: "IIITD&MK",
+      residence: "Warangal",
+      company: "Agilis AS",
+      role: "Front End Engineer",
+      contactNumber: "9566181750",
+      emailId: "vangetikarthik26@gmail.com",
+    },
+  });
+});
 /**
  * Server Activation
  */
